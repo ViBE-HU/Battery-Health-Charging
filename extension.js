e7dce1d934fd37655f6b9a81bac322e82c8bc998
maniacx <macs18max@gmail.com>
2023-05-01 18:27:09 -0500

    Battery Health Charging: Use unlock-dialog session mode

--- extension.js
+++ extension.js
@@ -11,25 +11,45 @@ const PowerIcon = shellVersion > 42 ? Me.imports.lib.powerIcon : Me.imports.lib.
 
 var thresholdPanel = null;
 var powerIcon = null;
+let sessionId = null;
 
 function init() {
     ExtensionUtils.initTranslations(Me.metadata.uuid);
 }
 
+// thresholdPanel created in user-mode session and destroyed in lockscreen session
+// powerIcon need to be enabled in lockscreen session to apply correct icon to Gnome System Battery indicator icon in System-tray.
 function enable() {
-    thresholdPanel = new Panel.ThresholdPanel();
-    if (powerIcon === null)
-        powerIcon = new PowerIcon.BatteryStatusIndicator();
+    // Do not create threshold panel if enable is triggered in lockscreen state
+    if (!Main.sessionMode.isLocked && thresholdPanel === null)
+        thresholdPanel = new Panel.ThresholdPanel();
+
+    powerIcon = new PowerIcon.BatteryStatusIndicator();
+
+   // Destroy thresholdPanel on unlock-dialog / create thresholdPanel in user-mode
+    sessionId = Main.sessionMode.connect('updated', session => {
+        if (session.currentMode === 'user' || session.parentMode === 'user') {
+            if (thresholdPanel === null)
+                thresholdPanel = new Panel.ThresholdPanel();
+        }  else if (session.currentMode === 'unlock-dialog') {
+            thresholdPanel.destroy();
+            thresholdPanel = null;
+        }
+    });
 }
 
 function disable() {
-    thresholdPanel.destroy();
-    thresholdPanel = null;
+    if (thresholdPanel !== null) {
+        thresholdPanel.destroy();
+        thresholdPanel = null;
+    }
 
-    // Battery indicator icon in System-tray is also used in lockscreen
-    // Do not disable powerIcon in lockscreen
-    if (!Main.sessionMode.isLocked) {
-        powerIcon.disable();
-        powerIcon = null;
+    powerIcon.disable();
+    powerIcon = null;
+    
+    if (sessionId) {
+        Main.sessionMode.disconnect(sessionId);
+        sessionId = null;
     }
 }
+
